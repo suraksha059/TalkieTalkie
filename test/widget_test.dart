ce4beb80d619cff4b5ac01b4f8e5dd3602297193
Suraksha059 <surakshyat09@gmail.com>
2026-06-07 13:09:02 +0545

    Resolved the problems

--- test/widget_test.dart
+++
@@ -1,7 +0,0 @@
-// This is a placeholder test file.
-// Widget tests will require Firebase mocking setup.
-// For now, this file is intentionally left minimal.
-
-void main() {
-  // TODO: Add widget tests with mocked Firebase
-}
